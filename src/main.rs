d5491a46846d8241184ec8e5a0ad46b07c1c6e9a
AdrienDML <adriendml99@protonmail.com>
2023-07-22 14:59:14 +0200

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello lol")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    }).bind(("localhost", 8080))?
+    .run()
+    .await
+}
